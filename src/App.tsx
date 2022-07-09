dd69c3a2f7d0d761d52bbcbe7b3fc30eeeb16322
pie7 <j3pxy6@gmail.com>
2022-07-09 11:59:26 +0800

    refactor: add List component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useFetch } from "./hooks/useFetch";
+import * as API from "./apis";
+import List from "./components/List";
+
+export const AppContext = React.createContext({
+  data: {
+    items: []
+  },
+  isLoading: false
+})
 
 function App() {
+  const { isLoading, data } = useFetch(API.photos_public())
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppContext.Provider value={{ isLoading, data }}>
+        <div className='container m-auto mt-10 overflow-hidden'>
+          <List />
+        </div>
+      </AppContext.Provider>
     </div>
   );
 }
