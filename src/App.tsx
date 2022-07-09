66858c21f70f11ce77a51120adf497413d589aab
pie7 <j3pxy6@gmail.com>
2022-07-09 15:40:08 +0800

    feat: add search keyword feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { useFetch } from "./hooks/useFetch";
 import * as API from "./apis";
 import List from "./components/List";
+import SearchBox from "./components/SearchBox";
 
 export const AppContext = React.createContext({
   data: {
@@ -11,13 +12,33 @@ export const AppContext = React.createContext({
   isLoading: false
 })
 
+
 function App() {
-  const { isLoading, data } = useFetch(API.photos_public())
+  const [inputText, setInputText] = useState('')
+  const [searchKeyword, setSearchKeyword] = useState('')
+  const { isLoading, data } = useFetch(API.photos_public(searchKeyword))
+
+  const updateSearchInput = (e: any) => {
+    setInputText(e.target.value)
+  }
+  const triggerSearch = (searchKeyword: string) => {
+    setSearchKeyword(searchKeyword)
+    setInputText('')
+  }
   return (
     <div className="App">
       <AppContext.Provider value={{ isLoading, data }}>
         <div className='container m-auto mt-10 overflow-hidden'>
-          <List />
+          <SearchBox
+            searchKeyword={inputText}
+            updateSearchInput={updateSearchInput}
+            triggerSearch={triggerSearch}
+          />
+          {isLoading ?
+            <div>Is Loading...</div>
+            :
+            <List />
+          }
         </div>
       </AppContext.Provider>
     </div>
