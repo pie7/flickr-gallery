a6f2095d184c5789a4fabe83b1d2b75a60013479
pie7 <j3pxy6@gmail.com>
2022-07-11 16:22:27 +0800

    fix: use fetchJsonp

--- src/apis.ts
+++ src/apis.ts
@@ -1,5 +1,5 @@
 export const photos_public = (tags = "") => `
-    https://www.flickr.com/services/feeds/photos_public.gne?format=json&nojsoncallback=1${
+    https://www.flickr.com/services/feeds/photos_public.gne?format=json${
       tags ? `&tags=${tags}` : ""
     }
 `;
--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,10 +1,13 @@
+import fetchJsonp from "fetch-jsonp";
 import { useState, useEffect } from "react";
 export const useFetch = (path = "") => {
   const [isLoading, setIsLoading] = useState(false);
   const [data, setData] = useState({ items: [] });
   useEffect(() => {
     setIsLoading(true);
-    fetch(path)
+    fetchJsonp(path, {
+      jsonpCallbackFunction: "jsonFlickrFeed",
+    })
       .then((res) => res.json())
       .then((data) => {
         setData(data);
