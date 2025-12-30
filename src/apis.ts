682d632f9dc3048fe2d81e9922e4688bf93f66c1
pie7 <j3pxy6@gmail.com>
2025-12-30 12:05:56 +0900

    fix: default tags value nature

--- src/apis.ts
+++ src/apis.ts
@@ -1,5 +1,5 @@
 export const photos_public = (tags = "") => `
     https://www.flickr.com/services/feeds/photos_public.gne?format=json${
-      tags ? `&tags=${tags}` : ""
+      tags ? `&tags=${tags}` : "&tags=nature"
     }
 `;
